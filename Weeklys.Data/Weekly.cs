6725cf3729d43415670589b1970977a20d2aa441
Robert Hammer <bobbyhammer58@outlook.com>
2021-04-15 19:28:54 -0400

    Database and Connection setup

---
+++ Weeklys.Data/Weekly.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weeklys.Data
+{
+    public class Weekly
+    {
+        [Key]
+        public int WeeklyID { get; set; }
+
+        [Required]
+        public double Revenue { get; set; }
+
+        [Required]
+        public double Expenses { get; set; }
+
+        public double? TaxesSum { get; set; }
+
+        public double? Profit { get; set; }
+
+        [Required]
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
