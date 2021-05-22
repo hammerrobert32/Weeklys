198dc9a383c046b13cf3084a8c6f920a174bcdff
Robert Hammer <bobbyhammer58@outlook.com>
2021-05-21 20:35:10 -0400

    Added Paychecks models

---
+++ Weeklys.Models/PaychecksCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weeklys.Models
+{
+    public class PaychecksCreate
+    {
+        public Enum Name { get; set; }
+
+        [Required]
+        public double AmountPaid { get; set; }
+    }
+}
---
+++ Weeklys.Models/PaychecksListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weeklys.Models
+{
+    public class PaychecksListItem
+    {
+        public int PaychecksID { get; set; }
+
+        public Enum Name { get; set; }
+
+        public double AmountPaid { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+    }
+}
